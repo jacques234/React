d196eec85b46f2a2ac5f620efca58c8f68dc183f
Diego Perez <perezmolinadiego030@gmail.com>
2024-08-18 10:24:54 -0600

    Primeros proyectos

---
+++ proyects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,16 @@
+export function App(){
+    return (
+        <article style={{display:'flex',color:'#fff'}}>
+            <header>
+                <img alt="Avatar" src="https://pbs.twimg.com/profile_images/1433234061691084800/8TUii2rl_normal.jpg" />
+                <div>
+                    <strong>AMLO</strong>
+                    <span>@Amlo</span>
+                </div>
+            </header>
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
